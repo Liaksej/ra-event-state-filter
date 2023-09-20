ea228e47aae79a4fa993dcae2a2ab5e409dd225d
Liaksej <tarusov.liaksej@gmail.com>
2023-09-20 19:54:36 +0200

    Add 'ProjectList' component for project filtering
    Introduced a new 'ProjectList' component in ProjectList.tsx file. This component accepts a list of projects and a selected category as props and displays only the projects belonging to the selected category. If state value is 'All', it displays all projects. The purpose of this component is to provide filter functionality to the projects based on their categories.

---
+++ src/components/ProjectList.tsx
@@ -0,0 +1,30 @@
+import { FunctionComponent } from "react";
+import Masonry from "@mui/lab/Masonry";
+
+interface ProjectListProps {
+  projects: {
+    img: string;
+    category: string;
+  }[];
+  state: string;
+}
+
+export const ProjectList: FunctionComponent<ProjectListProps> = ({
+  projects,
+  state,
+}) => {
+  const images =
+    state === "All"
+      ? projects
+      : projects.filter((project) => project.category === state);
+
+  return (
+    <Masonry columns={3} spacing={0}>
+      {images.map((image, index) => (
+        <div key={index} className="p-1">
+          <img className="w-64 h-full" src={image.img} alt={"image"} />
+        </div>
+      ))}
+    </Masonry>
+  );
+};
